08eba3a3356efb9bc1aad45cf5477c514f890ad1
Jakub Kozłowski <kubukoz@gmail.com>
2025-08-02 22:29:23 +0200

    Attempt to set proper base url in github

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from "vite";
 import scalaJSPlugin from "@scala-js/vite-plugin-scalajs";
 
+const baseUrl = process.env.GITHUB_REPOSITORY
+  ? "/" + process.env.GITHUB_REPOSITORY.split("/")[1]
+  : "/TODO";
+
+console.log("Base URL:", baseUrl);
+
 export default defineConfig({
   plugins: [scalaJSPlugin({ cwd: "../", projectID: "web" })],
   server: {
@@ -11,5 +17,5 @@ export default defineConfig({
       },
     },
   },
-  base: "/TODO",
+  base: baseUrl,
 });
