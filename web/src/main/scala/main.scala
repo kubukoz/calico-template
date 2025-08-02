c6a73671e07986f48eea85f144f3c59048b04b50
Jakub Kozłowski <kubukoz@gmail.com>
2025-08-02 22:38:08 +0200

    Add something interesting to the initial setup

--- web/src/main/scala/main.scala
+++ web/src/main/scala/main.scala
@@ -4,8 +4,21 @@ import calico.html.io.given
 import cats.effect.IO
 import cats.effect.kernel.Resource
 import fs2.dom.HtmlElement
+import org.scalajs.dom.MouseEvent
 
 object App extends IOWebApp {
 
-  def render: Resource[IO, HtmlElement[IO]] = div("hello there")
+  def render: Resource[IO, HtmlElement[IO]] = div(
+    h2("hello there!"),
+    "Mouse coords: ",
+    fs2
+      .dom
+      .events[IO, MouseEvent](org.scalajs.dom.window, "mousemove")
+      .map { e =>
+        e.screenX -> e.screenY
+      }
+      .holdResource((0d, 0d))
+      .map(_.map(_.toString)),
+  )
+
 }
